56055798794e728cc4506a92bb2f46ef87111ebc
racts <601935662@qq.com>
2022-05-21 23:37:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var screenshot = require('desktop-screenshot');
+
+screenshot("./screenshot/screenshot.png", function(error, complete) {
+    if(error)
+        console.log("Screenshot failed", error);
+    else
+        console.log("Screenshot succeeded");
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+## 电脑屏幕截图
+### 运行: 
+`node ./index.js` 
+### 生成的截图在screenshot目录下 
+### 参考： 
+[desktop-screenshot](https://github.com/johnvmt/node-desktop-screenshot)
